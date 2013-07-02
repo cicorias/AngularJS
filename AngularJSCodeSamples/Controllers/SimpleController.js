863c0b4c2a1a7c53047085d98be7ce9a26dda9fa
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-07-02 05:44:11 +0100

    Controller - View Binding example added

---
+++ AngularJSCodeSamples/Controllers/SimpleController.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../angular.min.js" />
+
+function SimpleController($scope) {
+    $scope.techies = [
+        { name: 'Prasad Honrao', company: 'Cognizant' },
+        { name: 'Colin Gray', company: 'JPMC' },
+        { name: 'Scott Allen', company: 'OdeToCode' },
+        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+        { name: 'Scott Hanselman', company: 'Microsoft' },
+        { name: 'John Papa', company: 'Pluralsight' }
+    ];
+}
