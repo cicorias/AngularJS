6cf3c244ae506bdf732c5388f943d50147fdd6bd
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-07-03 10:18:47 +0100

    AngularJS Module example added

---
+++ AngularJSCodeSamples/Modules/SimpleModule.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../angular.min.js" />
+var simpleModule = angular.module('SimpleModule', []);
+
+simpleModule.controller('SimpleController', function ($scope) {
+    $scope.techies = [
+        { name: 'Prasad Honrao', company: 'Cognizant' },
+        { name: 'Colin Gray', company: 'JPMC' },
+        { name: 'Scott Allen', company: 'OdeToCode' },
+        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+        { name: 'Scott Hanselman', company: 'Microsoft' },
+        { name: 'John Papa', company: 'Pluralsight' }
+    ];
+})
+
+
+// Another way to define module and controller binding
+
+//function SimpleController($scope) {
+//    $scope.techies = [
+//        { name: 'Prasad Honrao', company: 'Cognizant' },
+//        { name: 'Colin Gray', company: 'JPMC' },
+//        { name: 'Scott Allen', company: 'OdeToCode' },
+//        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+//        { name: 'Scott Hanselman', company: 'Microsoft' },
+//        { name: 'John Papa', company: 'Pluralsight' }
+//    ];
+//}
+//simpleModule.controller(SimpleController);
+
