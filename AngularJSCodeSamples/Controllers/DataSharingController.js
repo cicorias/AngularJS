34a75299e1cc1233961b22777fb595333deb4048
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-07-04 09:27:42 +0100

    Controller data sharing example added

---
+++ AngularJSCodeSamples/Controllers/DataSharingController.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../angular.min.js" />
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function () {
+    return { message: "Data retrieved from service" }
+})
+
+
+function FirstController($scope, Data) {
+    $scope.data = Data;
+}
+
+function SecondController($scope, Data) {
+    $scope.data = Data;
+}
