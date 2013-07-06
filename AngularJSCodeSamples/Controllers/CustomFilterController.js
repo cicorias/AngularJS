80f65df4d41bf8d7f31b76ffbedd33404e30c932
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-07-06 22:49:02 +0100

    Added custom directive / filter example

---
+++ AngularJSCodeSamples/Controllers/CustomFilterController.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../angular.min.js" />
+
+var customFilterApp = angular.module('customFilterApp', []);
+customFilterApp.filter('reverse', function () {
+    return function (text) {
+        return text.split('').reverse().join('');
+    }
+}
+);
+
+function CustomFilterController($scope) {
+
+    $scope.message = "Hello";
+
+}
