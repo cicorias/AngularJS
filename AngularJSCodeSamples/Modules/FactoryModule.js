d5be9cbe22c50c2a2eee01fb8c2754b372a386ca
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-07-08 07:08:36 +0100

    Factory example added

---
+++ AngularJSCodeSamples/Modules/FactoryModule.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../angular.min.js" />
+
+var employeeModule = angular.module('EmployeeModule', []);
+
+employeeModule.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', { templateUrl: '../../pages/12Factory.html', controller: 'EmployeeController' })
+});
+
+employeeModule.factory('employeeFactory', function () {
+
+    var employees = [
+        { name: 'Prasad Honrao', company: 'Cognizant' },
+        { name: 'Colin Gray', company: 'JPMC' }
+    ];
+
+    var factory = {};
+
+    factory.getEmployees = function () {
+        return employees;
+    }
+
+    return factory;
+});
+
+employeeModule.controller('EmployeeController', function ($scope, employeeFactory) {
+    $scope.Employees = employeeFactory.getEmployees();
+});
