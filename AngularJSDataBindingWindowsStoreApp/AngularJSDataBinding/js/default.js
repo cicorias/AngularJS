53fdb780f822b9dd87ef25506b1f22e2ebed60ae
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-05 00:52:18 +0100

    Base project created

---
+++ AngularJSDataBindingWindowsStoreApp/AngularJSDataBinding/js/default.js
@@ -0,0 +1,18 @@
+﻿function TechieController($scope) {
+    $scope.Techies = [
+        { name: 'Prasad Honrao', company: 'Cognizant' },
+        { name: 'Colin Gray', company: 'JPMC' },
+        { name: 'Scott Allen', company: 'OdeToCode' },
+        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+        { name: 'Scott Hanselman', company: 'Microsoft' },
+        { name: 'John Papa', company: 'Pluralsight' }
+    ];
+
+    $scope.addTechie = function () {
+        $scope.Techies.push({
+            name: $scope.newTechie.name,
+            company: $scope.newTechie.company
+        });
+    }
+}
+
