6ed1b4eaf3c60b77378961c29cb7600dbbe28589
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-07 15:32:48 +0100

    Controller binding defined

--- AngularJSWinJSListViewBinding/AngularJSWinJSListViewBinding/js/default.js
+++ AngularJSWinJSListViewBinding/AngularJSWinJSListViewBinding/js/default.js
@@ -3,26 +3,26 @@
 
     var app = WinJS.Application;
 
-    var employees = [
-        { name: 'Scott Allen', company: 'OdeToCode' },
-        { name: 'Dan Wahlin', company: 'The Wahlin Group' },
-        { name: 'Scott Hanselman', company: 'Microsoft' },
-        { name: 'John Papa', company: 'Pluralsight' },
-    ];
+    //var employees = [
+    //    { name: 'Scott Allen', company: 'OdeToCode' },
+    //    { name: 'Dan Wahlin', company: 'The Wahlin Group' },
+    //    { name: 'Scott Hanselman', company: 'Microsoft' },
+    //    { name: 'John Papa', company: 'Pluralsight' },
+    //];
 
-    WinJS.Namespace.define("Data", {
-        employees: employees
-    });
+    //WinJS.Namespace.define("Data", {
+    //    employees: employees
+    //});
 
     app.onactivated = function (args) {
         WinJS.UI.processAll();
     };
 
-    app.onready = function () {
-        var dataList = new WinJS.Binding.List(Data.employees);
-        var employeesList = document.getElementById('employeesListView').winControl;
-        employeesList.itemDataSource = dataList.dataSource;
-    }
+    //app.onready = function () {
+    //    var dataList = new WinJS.Binding.List(Data.employees);
+    //    var employeesList = document.getElementById('employeesListView').winControl;
+    //    employeesList.itemDataSource = dataList.dataSource;
+    //}
 
     app.start();
 })();
